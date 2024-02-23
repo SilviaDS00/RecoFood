e8a16cafe7321a06fe2b271344f3d5adb4876524
Elena Racero González <146923466+ElenaRacero3@users.noreply.github.com>
2024-02-23 11:42:29 +0100

    Implementación chatbot

--- Django/app/views.py
+++ Django/app/views.py
@@ -1,19 +1,62 @@
+""" 
+Hay que instalar esto antes de ejecutar:
+    - pip install django djangorestframework
+    - pip install django-cors-headers
+    - pip install google.generativeai
+    - pip install dotenv
+    - Arrancar el Servidor: python manage.py runserver 
+"""
+
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse, HttpResponse
-from .predict import hacer_prediccion
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+import os
+from dotenv import load_dotenv
+import google.generativeai as gen_ai
+
+# Load environment variables
+load_dotenv()
+
+GOOGLE_API_KEY = "AIzaSyBgMaYQkaDOv-4OGykVdXLPZcTrN9dM-WY"
+GOOGLE_API_KEY1 = os.getenv("GOOGLE_API_KEY")
+
+# Set up Google Gemini-Pro AI model
+gen_ai.configure(api_key=GOOGLE_API_KEY1)
+model = gen_ai.GenerativeModel("gemini-pro")
+
+# Start chat session
+chat_session = model.start_chat(history=[])
+
+
+# Function to translate roles between Gemini-Pro and Streamlit terminology
+def translate_role(user_role):
+    if user_role == "model":
+        return "assistant"
+    else:
+        return user_role
+
 
 @csrf_exempt
-def endpoint_prediccion(request):
-    if request.method == 'POST':
-        datos_de_entrada = request.POST.get('datos_de_entrada')  # Ajusta según tu formato de datos
-        # Realiza cualquier preprocesamiento necesario en tus datos de entrada
+def chatbot_view(request):
+    if request.method == "GET":
+        response_data = {
+            "message": "Hola. Has realizado una solicitud GET a la página de inicio."
+        }
+        return JsonResponse(response_data)
 
-        # Hacer la predicción
-        prediccion = hacer_prediccion(datos_de_entrada)
+    elif request.method == "POST":
+        # Get user prompt from POST data
+        user_prompt = request.POST.get("prompt", "")
+        if user_prompt:
+            # Send user's message to Gemini-Pro and get the response
+            gemini_response = chat_session.send_message(user_prompt)
 
-        # Devuelve la predicción como respuesta JSON
-        return JsonResponse({'prediccion': prediccion})
+            # Return Gemini-Pro's response
+            response_data = {"message": gemini_response.text}
+        else:
+            response_data = {
+                "error": "No se proporcionó ningún prompt de usuario en la solicitud POST."
+            }
 
-    return JsonResponse({'error': 'Se esperaba una solicitud POST'})
-def home(request):
-    return JsonResponse({'message': '¡Bienvenido a la API de mi proyecto!'})
+        return JsonResponse(response_data)
+    else:
+        return HttpResponse(status=405)
