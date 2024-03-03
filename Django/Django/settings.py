d1861178cbe47ac8423f56d9184302cbf7c2dc18
Silvia Donaire <silviadonaireserrano@gmail.com>
2024-03-03 18:43:25 +0100

    feat: change static url path

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'assets'
 # Default primary key field type
